c10bf0cd1d2735e6ebbd3609876a2dcbdb545012
Rafael quadros <112786439+RafaelQuadros1@users.noreply.github.com>
2025-04-01 20:32:13 -0300

    remove unnecessary whitespace in create task form

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,10 +4,8 @@
         <h1>Criar Tarefa</h1>
         <label for="task">Task:</label>
         <input type="text" name="task" id="task" placeholder="Descrição da tarefa" required>
-
         <label for="description">Descrição:</label>
         <textarea name="description" id="description" placeholder="Detalhes da tarefa"></textarea>
-
         <button type="submit">Criar Tarefa</button>
     </form>
 </div>
